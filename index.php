424c759f0a05ad80b5863ef82c706233345a6657
1Haziq <haziqhusni59@gmail.com>
2024-12-26 15:18:19 +0800

    -Separated header(navbar)& footer(sitemap) from home screen

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>skillTrain</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <?php 
+      include('header.html');
+      include('footer.html');
+    ?>
+</head>
+<body>
+    <div class="container">
+        <main id="main">
+            sdLorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo corrupti itaque illum sapiente! Dicta et fugit magnam, nihil voluptates quod eius quis vitae aspernatur facere? Voluptatibus vero cupiditate odit cumque sunt repellendus similique eum eveniet earum eius excepturi corporis, maiores ducimus quasi assumenda omnis, placeat, aut in distinctio aliquid. Praesentium quidem harum magnam laboriosam sint aliquam non, possimus accusamus vel voluptatem incidunt, sequi debitis exercitationem consectetur qui excepturi suscipit hic, facilis modi nisi? Eos, rem? Beatae amet perspiciatis possimus corrupti dolore dolores porro eligendi, eaque ad, architecto quam est. Laudantium distinctio impedit culpa autem eaque perferendis ipsa nobis eius. Sint ipsum id tenetur earum repudiandae sequi laudantium magni obcaecati odit a hic necessitatibus fugiat deserunt iusto nulla assumenda, mollitia error impedit voluptates facilis nisi cum aut! Ex, expedita?
+        </main>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
