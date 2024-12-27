368fbfec8ebedea21b4df6455e559737e43d7883
1Haziq <haziqhusni59@gmail.com>
2024-12-27 23:22:32 +0800

    -Linked login page to index page -Added header to login page -Fixed icons on sitemap

--- login.php
+++ login.php
@@ -14,6 +14,7 @@
     <!-- Load Alpine.js (Defer Loading) -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
+    <?php include('header.html');?>
 
 </head>
 <body>
